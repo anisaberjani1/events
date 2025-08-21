5ef603fad1c5c25aac050f577db1fc95e27bb6cf
anisaberjani1 <anisa.berjani@gmail.com>
2025-08-21 04:54:07 +0200

    Events Page added

--- src/pages/events/Events.jsx
+++ src/pages/events/Events.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Collection from "../../components/shared/Collection";
+import { useAllEvents } from "../../hooks/useEvents";
 
 const Events = () => {
+  const { data: events, isLoading, isError } = useAllEvents();
+  if (isLoading) return <p className="wrapper">Loading events...</p>;
+  if (isError) return <p className="wrapper">Failed to load events.</p>;
   return (
     <section className="wrapper !my-8 flex flex-col gap-8 md:gap-12">
       <h2 className="h2-bold ">Find Your Next Adventure</h2>
@@ -10,7 +14,7 @@ const Events = () => {
           <CategoryFilter /> */}
       </div>
       <Collection
-        data={[]}
+        data={events}
         emptyTitle="No Events Found"
         emptyStateSubtext="Come back later"
         collectionType="All_Events"
